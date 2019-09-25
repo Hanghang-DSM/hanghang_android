6f10aaa9390465a76e2e2702dcbc6a1018866ac3
alton15 <alton15@naver.com>
2019-09-25 22:56:40 +0900

    [Add] Cody 액티비티 추가

---
+++ app/src/main/java/com/stac/hanghangtwo/MyCodyActivity.java
@@ -0,0 +1,30 @@
+package com.stac.hanghangtwo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyCodyActivity extends AppCompatActivity {
+
+    ImageView btn_make_cody;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_cody);
+
+        btn_make_cody = findViewById(R.id.btn_make_cody);
+        btn_make_cody.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyCodyActivity.this, MakeCodyActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
